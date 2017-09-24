d856315bafe89fe7fd94b6d44029abb6bc92aba1
Alexey Malofeev <ceo@mavlad.ru>
2017-09-24 20:03:53 +0400

    +webpack

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,4 @@
 /**
  * Created by UserMavlad on 24.09.2017.
  */
-module.exports = '<h1>title asdsad 222</h1>>'
+module.exports = '<h1>title asdsad 222</h1>>';
--- src/index.js
+++ src/index.js
@@ -1,14 +1,13 @@
 /**
  * Created by UserMavlad on 24.09.2017.
  */
-var $ = require('jquery');
-var str = require('./constants.js');
+const $ = require('jquery');
+const str = require('./constants.js');
 
-var App  = function() {
-
+const App = function() {
     $('body').html('<p>Paragraph</p>');
     document.write(str);
-
 };
 
-var App = new App();
+const app = new App();
+app.get = function() {};
